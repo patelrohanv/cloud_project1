6d3106a5654333b5fdf57d6f0eb5462cbf274795
Salvatore Avena <sja33@pitt.edu>
2017-03-26 17:47:58 -0400

    nothing really

--- IndexerDriver.java
+++ IndexerDriver.java
@@ -12,11 +12,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class IndexerDriver{
+public class indexerDriver{
     public static indexer(Path input, Path output){
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "inverted index");
-        job.setJarByClass(IndexerDriver.class);
+        job.setJarByClass(indexerDriver.class);
         job.setMapperClass(indexerMapper.class);
         job.setCombinerClass(indexerReducer.class);
         job.setReducerClass(indexerReducer.class);
