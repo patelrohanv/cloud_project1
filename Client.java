56e88a628cc107839a440874eab568d5af9b4790
patelrohanv <rohanpatel@Rohans-MacBook-Pro.local>
2017-03-26 15:54:18 -0400

    updated client

--- Client.java
+++ Client.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Client{
     public static void main(String[] args){
@@ -9,6 +10,15 @@ public class Client{
             //System.out.println(f.toString());
         }
 
-
+        int input = 0;
+        Scanner kbd = new Scanner(System.in);
+        do{
+            if(input > 3 || input < 1){
+                System.out.println("Not a valid option");
+                continue;
+            }
+            System.out.println("Enter an option:\n\t1. Search for a word \n\t2. Add a document \n\t3. Quit");
+            input = kbd.nextInt();
+        }while(input != 3);
     }
 }
