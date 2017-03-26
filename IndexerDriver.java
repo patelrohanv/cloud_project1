98ec6808dd0d6f8e1c4a1c198711a49a4c383ebf
patelrohanv <rohanpatel@Rohans-MacBook-Pro.local>
2017-03-26 17:45:34 -0400

    Driver

---
+++ IndexerDriver.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.commons.lang3.tuple.Pair<L,R>;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class IndexerDriver{
+    public static indexer(Path input, Path output){
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "inverted index");
+        job.setJarByClass(IndexerDriver.class);
+        job.setMapperClass(indexerMapper.class);
+        job.setCombinerClass(indexerReducer.class);
+        job.setReducerClass(indexerReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
