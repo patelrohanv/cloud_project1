2dad5da94532966ee34ad9c4863ab81e0e8328a7
Salvatore Avena <sja33@pitt.edu>
2017-03-26 16:11:59 -0400

    Started working on indexerMapper

--- indexerMapper.java
+++ indexerMapper.java
@@ -1,15 +1,41 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.*;
+import java.math.*;
 
-package com.hadoop.example;
-
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class indexerMapper extends Mapper<LongWritable, Text, Text, Text> {
-  procedure MAP(did id, doc d) {
-    H new AssociativeArray forall term t in doc d do
-      H{t <-- ßH{t} + 1
-    forall term t in H do
-      EMIT( tuple <t, d> , tf H{t} )
+public class indexerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+  private IntWritable one = new IntWritable(1);
+  private Text term = new Text();
+
+  public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    Map H = new HashMap();
+    StringTokenizer itr = new StringTokenizer(value.toString());
+
+    while (itr.hasMoreTokens()) {
+      term.set(itr.nextToken());
+      if (!H.containsKey(term))
+      H.put(term, one);
+      else
+      H.put(term, H.get(term) + 1);
+    }
+
+    for (Map.Entry<String, IntWritable> entry : H.entrySet()) {
+      String key = entry.getKey();
+      IntWritable freq = entry.getValue();
+      IntWritable tf = 1 + Math.log(value);
+      //SOMETUPLE key = dId and Term
+      context.write(key, tf);
+    }
   }
 }
