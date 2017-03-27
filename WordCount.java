3fd732953729b538eaa8716d6ef732cc7cfadb4a
Salvatore Avena <sja33@pitt.edu>
2017-03-27 19:38:25 -0400

    first part works

--- WordCount.java
+++ WordCount.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+//import org.apache.opennlp.tools.stemmer.PorterStemmer;
+//import org.apache.lucene.analysis.PorterStemmer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -8,6 +10,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -22,13 +25,22 @@ public class WordCount {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
-                token = its.nextToken();
-                token = token.toLowerCase().replaceAll("[^a-zA-Z]", "");
-                token = token+"#"+fileName;
-                private String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+                token = preProcess(itr.nextToken());
+                //token = itr.nextToken();
+                String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+                token = token+"^"+fileName;
                 word.set(token);
                 context.write(word, one);
             }
+        }
+        public String preProcess(String s) {
+            String stemmed, lower, alpha;                       //
+            //PorterStemmer stemmer = new PorterStemmer();        //
+            //stemmed = stemmer.stem(s);                          //perform stemming
+            //lower = stemmed.toLowerCase();                      //convert to lowercase
+            lower = s.toLowerCase();                            //convert to lowercase
+            alpha = lower.replaceAll("[^a-zA-Z]", "");          //retain only alphabet chars
+            return alpha;
         }
     }
 
